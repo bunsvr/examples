8e0e986eab2d7bd26cbb29d302d5941ac29b81c6
aquapi <aquaplmc@gmail.com>
2024-01-03 22:23:49 +0700

    Fixing and testing

--- chat/src/routes/room/index.routes.ts
+++ chat/src/routes/room/index.routes.ts
@@ -5,15 +5,18 @@ import { stat, html } from '@stricjs/app/send';
 import route from './index.ws';
 
 // Get the room ID
-const getID = qs.searchKey('id'), userLimit = 1e5 + 7;
+const getID = qs.searchKey('id'), userLimit = 1e6 + 7;
 
 export default routes('/room')
     .state(getID)
+
     // Render room page
     .get('/', c => html(render({ id: c.state })))
+
     // Websocket endpoint
     .get('/ws', c => route.upgrade(c, {
         data: { name: `User${Date.now() % userLimit}`, room: c.state }
     }))
+
     // Fallback
     .reject(() => stat('A room ID must be provided!', 400));
